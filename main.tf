8c66bf95fc329049c7aec8efa8f5caeca7335e11
Irfan Rizkianto Pratama <irfan@Irfan-MacBook-Air.local>
2023-02-27 17:33:33 +0700

    test

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ terraform {
     key            = "workload/terraform-workshop/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    role_arn       = "arn:aws:iam::454003166800:role/tf-exec-s3"
   }
 }
 
