9fcbae3cb788757ba8041253feebe1e5ff97dac7
Irfan Rizkianto Pratama <irfan@Irfan-MacBook-Air.local>
2023-02-27 15:28:17 +0700

    test

--- main.tf
+++ main.tf
@@ -1,20 +1,19 @@
 
 terraform {
   backend "s3" {
-    bucket  = "my-bucket-tfstate-2022"
-    key     = "workload/terraform-workshop/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-    role_arn = "arn:aws:iam::454003166800:role/tf-exec-s3"
-    profile                  = "default"
-    shared_credentials_files = ["/Users/irfan/.aws/credentials"]
+    bucket         = "my-bucket-tfstate-2022"
+    key            = "workload/terraform-workshop/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    role_arn       = "arn:aws:iam::454003166800:role/tf-exec-s3"
+    dynamodb_table = "my-table-tfstate-2022-lock"
+
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region                   = "us-east-1"
-  
+  region = "us-east-1"
 }
 
 
