c9c8be2357ded02a525bb63f0799cd3b89eb9e61
Irfan Rizkianto Pratama <irfan@Irfan-MacBook-Air.local>
2023-02-27 15:19:02 +0700

    add main.tf

--- main.tf
+++ main.tf
@@ -5,16 +5,16 @@ terraform {
     key     = "workload/terraform-workshop/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
-
     role_arn = "arn:aws:iam::454003166800:role/tf-exec-s3"
+    profile                  = "default"
+    shared_credentials_files = ["/Users/irfan/.aws/credentials"]
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
   region                   = "us-east-1"
-  shared_credentials_files = ["/Users/irfan/.aws/credentials"]
-  profile                  = "default"
+  
 }
 
 
