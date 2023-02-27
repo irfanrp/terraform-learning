e3e67e0a0e3b1a7b8759f2c62b67ac8fb34b80ce
Irfan Rizkianto Pratama <irfan@Irfan-MacBook-Air.local>
2023-02-27 19:48:17 +0700

    fixing version

--- security-groups.tf
+++ security-groups.tf
@@ -1,6 +1,6 @@
 module "ec2_bastion_security_group" {
   source = "terraform-aws-modules/security-group/aws"
-  version = "4.17.1"
+  version = "3.17.0"
 
   name        = var.ec2_bastion_security_group["name"]
   description = var.ec2_bastion_security_group["description"]
